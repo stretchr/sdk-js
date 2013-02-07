bbae62e10b5bc1d323fb144192036db1eb8bfe76
Mat Ryer <matryer@mac>
2013-02-06 17:24:42 -0700

    added testability

--- src/stretchr-testing.js
+++ src/stretchr-testing.js
@@ -33,9 +33,136 @@ if (typeof Stretchr == "undefined") {
 	throw "Stretchr: You must include the stretchr.js file as well as stretchr-testing.js"
 }
 
-Stretchr.NewTestSession = function(){
+/*
+	TestResponse is a test response object that will be returned to callbacks
+	if actual code is NOT.
+*/
+Stretchr.TestResponse = {
+	"~s": 200
+};
+
+/*
+	ResponseDelay is the amount of time to wait before responding.  If zero, calls
+	will be made immediately and before returning (recommended for unit testing).
+*/
+Stretchr.ResponseDelay = 0;
+
+/*
+	TestRequest is a test Request object that doesn't actually make any
+	real requests.
+*/
+Stretchr.GetTestRequest = function(session, path){
+
+	r = Stretchr.NewRequest(session, path)
+
+	r.recordRequest = function(){
+
+		this.path = r._path
+
+		this.where = {};
+
+		// remove the :
+		for (param in this._filterparams) {
+			this.where[param.slice(1, param.length)] = this._filterparams[param]
+		}
+
+		this.with = this._params;
+
+		// add to the request array
+		this._session.requests.push(this);
+
+		// if there is an onCompleted callback, call it with the next
+		// pre-defined response
+		if (this.onCompleted) {
+			if (this._session.responses.length > 0) {
+
+				if (Stretchr.ResponseDelay > 0) {
+					var _this = this
+					window.setTimeout(function(){
+				  	_this.onCompleted(_this._session.responses.pop())
+					}, Stretchr.ResponseDelay);
+				} else {
+					this.onCompleted(this._session.responses.pop())
+				}
+
+			} else {
+				// call the callback with the Stretchr.TestResponse
+				this.onCompleted(Stretchr.TestResponse)
+			}
+		}
+
+	}
+
+	r.create = function(callback){
+		this.onCompleted = callback;
+		this.action = "create";
+		this.recordRequest();
+		return this;
+	}
+	r.read = function(callback){
+		this.onCompleted = callback;
+		this.action = "read";
+		this.recordRequest();
+		return this;
+	}
+	r.update = function(callback){
+		this.onCompleted = callback;
+		this.action = "update";
+		this.recordRequest();
+		return this;
+	}
+	r.remove = function(callback){
+		this.onCompleted = callback;
+		this.action = "remove";
+		this.recordRequest();
+		return this;
+	}
+	r.replace = function(callback){
+		this.onCompleted = callback;
+		this.action = "replace";
+		this.recordRequest();
+		return this;
+	}
+
+	return r
+
+}
+
+/*
+	NewTestSession creates a Stretchr.Session object that will not make real
+	HTTP requests, allowing you to stub out responses and make assertions about the
+	calls made by your code.
+*/
+Stretchr.NewTestSession = function() {
 
 	s = Stretchr.NewSession("test", "public", "private")
+
+	/*
+		requests keeps track of all requests made.
+	*/
+	s.requests = [];
+
+	/*
+		responses holds a queue of the response objects to respond with.
+	*/
+	s.responses = [];
+
+	/*
+		respond sets up a response.  Responses will be returned in the order in which
+		they are called.
+	*/
+	s.respond = function(obj){
+
+		s.responses.push(obj)
+
+		// chain
+		return this;
+	}
+
+	// tell the at method to return a test request
+	s.at = function(path){
+		return Stretchr.GetTestRequest(this, path)
+	}
 
 	return s
 
--- tests/testing.js
+++ tests/testing.js
@@ -1,10 +1,78 @@
 buster.testCase("Test Session", {
 
-	"Test Session": function(){
+	"GetTestRequest & at": function(){
 
+		var s = Stretchr.NewTestSession()
+		var request = Stretchr.GetTestRequest(s, "path")
+
+		assert.defined(request, "at should return new request")
+		assert.equals(request._path, "path")
+		assert.equals(request._session, s, "session")
+
+		request = s.at("path")
+
+		assert.defined(request, "at should return new request")
+		assert.equals(request._path, "path")
+		assert.equals(request._session, s, "session")
+
+	},
+
+	"Test activity": function(){
+
+		Stretchr.ResponseDelay = 0;
 		var s = Stretchr.NewTestSession()
 
 		assert.defined(s, "NewTestSession")
+
+		var responseObject1 = {}
+		var responseObject2 = {}
+		var responseObject3 = {}
+
+		// set up some responses
+		s.respond(responseObject1).respond(responseObject2).respond(responseObject3)
+
+		assert.equals(s.responses.length, 3)
+
+		var res1, res2, res3, res4, res5;
+
+		// made some requests
+		s.at("people").where("age", ">30").read(function(r){
+			res1 = r;
+		});
+		s.at("people/123").create(function(r){
+			res2 = r;
+		});
+		s.at("people").with("~limit", 5).remove(function(r){
+			res3 = r;
+		});
+		s.at("people").update(function(r){
+			res4 = r;
+		});
+		s.at("people").replace(function(r){
+			res5 = r;
+		});
+
+		assert.equals(s.requests.length, 5)
+
+		assert.equals(s.requests[0].action, "read")
+		assert.equals(s.requests[0].path, "people")
+		assert.equals(s.requests[0].where["age"][0], ">30")
+		assert.equals(res1, responseObject1, "responseObject1")
+
+		assert.equals(s.requests[1].action, "create")
+		assert.equals(s.requests[1].path, "people/123")
+		assert.equals(res2, responseObject2, "responseObject2")
+
+		assert.equals(s.requests[2].action, "remove")
+		assert.equals(s.requests[2].path, "people")
+		assert.equals(s.requests[2].with["~limit"][0], 5)
+		assert.equals(res3, responseObject3, "responseObject3")
+
+		assert.equals(s.requests[3].action, "update")
+		assert.equals(res4, Stretchr.TestResponse, "responseObject1")
+
+		assert.equals(s.requests[4].action, "replace")
+		assert.equals(res5, Stretchr.TestResponse, "responseObject1")
 
 	}
 
