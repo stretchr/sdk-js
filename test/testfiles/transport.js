075b8b0c63c25bc03f7645daaef8e26cdd865366
Mat Ryer <mat@stretchr.com>
2013-09-17 15:13:39 -0600

    added Transport concept

---
+++ test/testfiles/transport.js
@@ -0,0 +1,46 @@
+var TestFileValue = 0;
+
+buster.testCase("JSONPTransport", {
+
+  "Transport.url": function(){
+
+    var t = new Stretchr.Transport();
+    t.setHost("monkey.something.com");
+    t.setProtocol("http");
+    t.setAPIVersion(1.1);
+
+    var u = t.url("/people/1?name=Ryan");
+
+    assert.equals(u, "http://monkey.something.com/api/v1.1/people/1?name=Ryan")
+
+  },
+
+  "JSONPTransport.makeRequest": function(){
+
+    var t = new Stretchr.JSONPTransport();
+    var options = {
+      method: "GET",
+      path: "testfiles/includes/IncreaseTestFileValueByOne.js"
+    };
+
+    var eventCallbacks = [];
+
+    // bind some events
+    t.before(function(){
+      eventCallbacks.push("before");
+    });
+    t.after(function(){
+      eventCallbacks.push("after");
+    });
+
+    t.makeRequest(options);
+
+    assert.equals(2, eventCallbacks.length, "Events should get called")
+
+    window.setTimeout(function(){
+      assert.equals(TestFileValue, 1);
+    }, 500);
+
+  }
+
+});
